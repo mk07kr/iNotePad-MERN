fb145c1157f4eef0647d294ffe8c92eedea0f64d
mk07kr <mk1309kr@gmail.com>
2024-12-28 15:56:11 +0530

    ADD NEW NOTE CREATED

--- backend/routes/note.js
+++ backend/routes/note.js
@@ -25,13 +25,17 @@ router.post('/newNote', fetchuser,[
     body("title","Enter a valid title").isLength({ min: 3 }),
     body("description","Description too short !").isLength({ min: 7 }),
 ], async (req, res) => {
+    const {title,description,tag}=req.body;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     try {
       
-      res.json(notes);
+      const note=new Note({
+        title,description,tag,user:req.user.id
+      });
+      const savedNote=await note.save();
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal Server Error");
