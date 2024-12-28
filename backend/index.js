2a205fbc3c9a6417a02520f0dfb6c0e278ae8f4b
mk07kr <mk1309kr@gmail.com>
2024-12-28 15:53:23 +0530

    CRUD Op Started

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const PORT = 5000;
 connectToMongo(); 
 
 app.use('/api/auth',require('./routes/auth'));
+app.use('/api/notes',require('./routes/note'));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- backend/routes/note.js
+++ backend/routes/note.js
@@ -1,12 +1,42 @@
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
+const { body, validationResult } = require("express-validator");
+const Note = require("../models/Notes"); 
 
-router.get('/',(req,res)=>{
-obj={
-    "id":"Hello MK",
-    num:77
-}
-res.json(obj);
-})
+//ROUTE 1:
+// GET /fetchAllNotes
+// Fetch all notes of the logged-in user
+router.get('/fetchAllNotes', fetchuser, async (req, res) => {
+  try {
+    // Find all notes belonging to the logged-in user
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
-module.exports=router
+//ROUTE 2:
+// POST /newNote
+// Create/Add New Notes of the logged-in user
+router.post('/newNote', fetchuser,[
+    body("title","Enter a valid title").isLength({ min: 3 }),
+    body("description","Description too short !").isLength({ min: 7 }),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      
+      res.json(notes);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+  
+
+module.exports = router;
