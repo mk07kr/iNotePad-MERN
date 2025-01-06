7f4fe9bc4f3f439fa4883415f13973cd4d7d3107
mk07kr <mk1309kr@gmail.com>
2025-01-07 01:50:30 +0530

    Note Item for notes preview

--- src/components/noteitem.js
+++ src/components/noteitem.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const noteitem = (props) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div class="card my-3">
+        <div class="card-body">
+          <h5 class="card-title">{note.title}</h5>
+          <p class="card-text">
+            {note.description}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default noteitem
+export default noteitem;
