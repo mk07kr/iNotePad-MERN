8eca1f83df502304fcc08ac3d4b7ce2aceef2d72
mk07kr <mk1309kr@gmail.com>
2025-01-08 01:20:10 +0530

    CRUD-> Add Note done

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,43 +1,64 @@
-import React from "react";
-
+import React, { useContext, useState } from "react";
+import noteContext from "../context/noteContext";
 const AddNote = () => {
+  const context = useContext(noteContext);
+  const { addNote } = context;
+  const [note, setNote] = useState({
+    title: "",
+    description: "",
+    tag: "default",
+  });
+  const handleClick = (e) => {
+    e.preventDefault();
+    addNote(note.title, note.description, note.tag);
+  };
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value });
+  };
   return (
-    <div>
-      <h2>Add Note</h2>
-      <form>
+    <div className="container my-3">
+      <h2>Add a Note</h2>
+      <form className="my-3">
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
-            Email address
+          <label htmlFor="title" className="form-label">
+            Title
           </label>
           <input
-            type="email"
+            type="text"
             className="form-control"
-            id="exampleInputEmail1"
+            id="title"
+            name="title"
             aria-describedby="emailHelp"
+            onChange={onChange}
           />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
-          </div>
         </div>
         <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
-            Password
+          <label htmlFor="description" className="form-label">
+            Description
           </label>
           <input
-            type="password"
+            type="text"
             className="form-control"
-            id="exampleInputPassword1"
+            id="description"
+            name="description"
+            onChange={onChange}
           />
         </div>
-
-        <button type="submit" className="btn btn-primary">
-          Submit
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={handleClick}>
+          Add Note
         </button>
       </form>
-
-      <Notes />
     </div>
   );
 };
-
 export default AddNote;
--- src/context/noteState.js
+++ src/context/noteState.js
@@ -3,18 +3,33 @@ import noteContext from "./noteContext";
 
 const NoteState = (props) => {
   // Read Notes
-  const noteInitial=[]
+  const noteInitial = [];
   const [notes, setNotes] = useState(noteInitial);
 
-   // Add a note
+  // Add a note
+  const addNote = (title, description, tag) => {
+    // TODO: API Call
+    console.log("Adding a new note");
+    const note = {
+      _id: "61322f119553781a8ca8d0e08",
+      user: "6131dc5e3e4037cd4734a0664",
+      title: title,
+      description: description,
+      tag: tag,
+      date: "2021-09-03T14:20:09.668Z",
+      __v: 0,
+    };
+    setNotes(notes.concat(note));
+  };
 
-   // Delete Note
+  // Delete Note
+  const deleteNote = () => {};
 
-   // Edit/Update Note
-
+  // Edit/Update Note
+  const editNote = () => {};
 
   return (
-    <noteContext.Provider value={{ notes, setNotes }}>
+    <noteContext.Provider value={{ notes, addNote, editNote, deleteNote }}>
       {props.children}
     </noteContext.Provider>
   );
