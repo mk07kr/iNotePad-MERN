3958b7c51a34e629168d0b443b0061b21038db5a
mk07kr <mk1309kr@gmail.com>
2025-01-07 21:34:18 +0530

    Add Note component seperated

--- src/components/home.js
+++ src/components/home.js
@@ -1,32 +1,12 @@
-
-import React from 'react';
-import Notes from './notes';
-
-
+import React from "react";
+import AddNote from "./AddNote";
 
 const Home = () => {
-  return(
-<div>
-  <h2>Add Note</h2>
-  <form>
-   
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1"/>
-  </div>
- 
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
-
-
-<Notes/>
- </div>
-  )
-}
+  return (
+    <div>
+      <AddNote />
+    </div>
+  );
+};
 
 export default Home;
