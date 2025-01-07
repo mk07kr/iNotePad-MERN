5b8001a983d6a5b27765da472a6447cc7936ca64
mk07kr <mk1309kr@gmail.com>
2025-01-07 21:28:20 +0530

    Alert updated

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,8 +3,14 @@ import React from "react";
 const Alert = (props) => {
   return (
     <div>
-      <div class="alert alert-success" role="alert">
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">
         {props.message}
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+        ></button>
       </div>
     </div>
   );
