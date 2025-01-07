0f2a4140c5279b5956cdb6d97107f0ca05afb52a
mk07kr <mk1309kr@gmail.com>
2025-01-07 21:30:50 +0530

    Note Item card updated with buttons

--- src/components/noteitem.js
+++ src/components/noteitem.js
@@ -7,8 +7,16 @@ const noteitem = (props) => {
       <div className="card my-3">
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
-          <div className="flex align-items-center"></div>
           <p className="card-text">{note.description}</p>
+          <div className="flex align-items-center">
+            <button className="btn btn-primary">
+              <i className="bi bi-pencil"></i> Edit
+            </button>
+            <button className="btn btn-danger">
+              <i className="bi bi-trash"></i> Delete
+            </button>
+
+          </div>
         </div>
       </div>
     </>
