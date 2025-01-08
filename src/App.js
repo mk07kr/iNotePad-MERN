d853a29f83f236036dd8a062a813f1c1ac7ff8aa
mk07kr <mk1309kr@gmail.com>
2025-01-08 22:58:27 +0530

    Login & Signup Page Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/home";
 import About from "./components/about";
-import NoteState from './context/noteState';
-
+import NoteState from "./context/noteState";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
@@ -15,11 +16,12 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route exact path="/about" element={<About />} />
-            <Route path="*" element={<h1>Page Not Found</h1>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
       </Router>
-      </NoteState>
+    </NoteState>
   );
 }
 
