70c35c3124535580d541ea507faece339651a9ed
mk07kr <mk1309kr@gmail.com>
2025-01-10 02:43:20 +0530

    HAiwa Show

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,74 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = (props) => {
-  const [credentials, setCredentials] = useState({ email: "", password: "" });
-  const navigate = useNavigate();
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: credentials.email,
-        password: credentials.password,
-      }),
-    });
-    const json = await response.json();
-    console.log(json);
-    if (json.success) {
-      // Save the auth token and redirect
-      localStorage.setItem("token", json.authtoken);
-      console.log(json.authtoken)
-      navigate("/");
-      props.showAlert("Login Successfully", "success");
-    } else {
-      props.showAlert("Invalid Credentials ! Try Again ", "danger");
-    }
-  };
-  const onChange = (e) => {
-    setCredentials({ ...credentials, [e.target.name]: e.target.value });
-  };
-  return (
-    <div>
-      <h2 className="mt-1 my-1">Login to dive into iNotebook</h2>
-      <form className="my-2" onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            value={credentials.email}
-            onChange={onChange}
-            id="email"
-            name="email"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
-          </div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            value={credentials.password}
-            onChange={onChange}
-            name="password"
-            id="password"
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+  
+  <div className="container">Hii I am login </div>
 };
 export default Login;
